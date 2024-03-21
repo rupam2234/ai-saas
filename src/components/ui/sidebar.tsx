7edd544ee781c12e3f305a5a36a601ad9d96edfc
rupam2234 <rupam.bharali99@gmail.com>
2024-03-21 17:18:07 +0530

    Sidebar | Nav | Auth

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,25 +1,25 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { Montserrat } from "next/font/google";
+import { Pacifico } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 
-const montserrat = Montserrat({
-  weight: "600",
+const pacifico = Pacifico({
+  weight: "400",
   subsets: ["latin"]
 });
 
 const Sidebar = () =>{
 
     return(
-        <div className="space-y-4 py-4 flex flex-col h-full text-white bg-slate-900">
+        <div className="space-y-2 py-3 flex flex-col h-full text-white bg-slate-900">
             <div className="py-1 px-3 flex-1">
               <Link href={"/dashboard"} className="flex items-center pl-3 mb-10">
-                <div className="relative w-10 h-10 mr-4">
+                <div className="relative w-9 h-9 mr-4">
                     <Image fill alt="logo" src={"/(media)/images/logo.png"}/>
                 </div>
-                <div className={cn("text-2xl font-bold text-white", montserrat.className)}>
+                <div className={cn("text-2xl font-bold text-gray-300", pacifico.className)}>
                     Nitro
                 </div>
               </Link>
